dd37cee557c56f74b1c5a398c651e1521f4c2199
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-15 02:17:24 +0300

    Clean up settings

--- selfstorage/urls.py
+++ selfstorage/urls.py
@@ -20,7 +20,6 @@ from storage import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("users.urls")),
     path("", views.index, name="index"),
     path("faq", views.faq, name="faq"),
     path("boxes", views.boxes, name="boxes"),
--- users/views.py
+++ users/views.py
@@ -1,5 +1,1 @@
 from django.shortcuts import render
-
-
-def dashboard(request):
-    return render(request, "users/dashboard.html")
