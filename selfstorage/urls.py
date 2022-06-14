5534a636df51e147c92c0c42a186e8341f3d6a98
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-14 14:54:59 +0300

    Add project `boxes` page

--- selfstorage/urls.py
+++ selfstorage/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", views.index, name="index"),
     path("faq", views.faq, name="faq"),
+    path("boxes", views.boxes, name="boxes"),
 ]
--- storage/views.py
+++ storage/views.py
@@ -7,3 +7,7 @@ def index(request):
 
 def faq(request):
     return render(request, "faq.html")
+
+
+def boxes(request):
+    return render(request, "boxes.html")
