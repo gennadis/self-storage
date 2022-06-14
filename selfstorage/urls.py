8ab0c3834810dee0709fa5c30664e2ffed4112b3
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-14 21:59:03 +0300

    Register new `users` app ulrs

--- selfstorage/urls.py
+++ selfstorage/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from storage import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", include("users.urls")),
     path("", views.index, name="index"),
     path("faq", views.faq, name="faq"),
     path("boxes", views.boxes, name="boxes"),
