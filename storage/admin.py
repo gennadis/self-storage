08a6d66410fd6fefd72f2e6a1ee12b0f3d5773ab
Govno <arhdam@yandex.ru>
2022-06-15 10:21:21 +0300

    Create Warehouse admin view

--- storage/admin.py
+++ storage/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.utils.html import format_html
 
-# Register your models here.
+from storage.models import WarehouseImage
+
+
+class ImageInline(admin.TabularInline):
+    model = WarehouseImage
+
+    fields = [
+        "image_file", "image_preview", "index",
+    ]
+
+    readonly_fields = [
+        "image_preview",
+    ]
+
+    def image_preview(self, obj):
+        preview_image_height = 200
+        return format_html("<img src='{0}' height={1} />", obj.image_file.url, preview_image_height)
+
+
+@admin.register
+class WarehouseAdmin(admin.ModelAdmin):
+    search_fields = ["address"]
