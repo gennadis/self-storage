2e3b104ff5e38d8fbd854155b1bf1a240837d42e
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-15 13:47:28 +0300

    Create custom uer admin panel

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,50 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = (
+        "phone_number",
+        "first_name",
+        "last_name",
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "phone_number",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email", "phone_number")
+    ordering = ("email",)
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
