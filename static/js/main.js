e6bf08b40822be864b7175d98ff4f1e640362635
Aosothra <arhdam@yandex.ru>
2022-06-16 11:17:32 +0300

    Fix wrong data substitution in lease acquirement form

--- static/js/main.js
+++ static/js/main.js
@@ -1,13 +1,16 @@
 document.addEventListener("DOMContentLoaded", ()=>{   
-    boxesTable = document.querySelector("#pills-all")
+    let selectedBox = undefined
+    let present_boxes = []
+    
+    let boxesTable = document.querySelector("#pills-all")
     boxesTable.replaceChildren()
 
-    warehouseButtons = document.querySelectorAll(".js-select-warehouse")
+    let warehouseButtons = document.querySelectorAll(".js-select-warehouse")
     for (warehouseButton of warehouseButtons){
         warehouseButton.addEventListener("click", fetch_data)
     }
 
-    buttonAllAreas = document.querySelector("#pills-all-tab")
+    let buttonAllAreas = document.querySelector("#pills-all-tab")
     buttonAllAreas.addEventListener("click", (event)=>{
         pills = document.querySelectorAll(".js-box-row")
         for (pill of pills){
@@ -15,7 +18,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }
     })
 
-    buttonTo3 = document.querySelector("#pills-to3-tab")
+    let buttonTo3 = document.querySelector("#pills-to3-tab")
     buttonTo3.addEventListener("click", (event)=>{
         pills = document.querySelectorAll(".js-box-row")
         for (pill of pills){
@@ -25,7 +28,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 pill.classList.add("d-none")
         }
     })
-    buttonTo10 = document.querySelector("#pills-to10-tab")
+    let buttonTo10 = document.querySelector("#pills-to10-tab")
     buttonTo10.addEventListener("click", (event)=>{
         pills = document.querySelectorAll(".js-box-row")
         for (pill of pills){
@@ -35,7 +38,7 @@ document.addEventListener("DOMContentLoaded", ()=>{
                 pill.classList.add("d-none")
         }
     })
-    buttonFrom10 = document.querySelector("#pills-from10-tab")
+    let buttonFrom10 = document.querySelector("#pills-from10-tab")
     buttonFrom10.addEventListener("click", (event)=>{
         pills = document.querySelectorAll(".js-box-row")
         for (pill of pills){
@@ -46,22 +49,23 @@ document.addEventListener("DOMContentLoaded", ()=>{
         }
     })
 
-    leaseDuration = document.querySelector("#leaseDuration")
-    leasePrice = document.querySelector("#leasePrice")
-    duration.addEventListener("change", (event)=>{
+    let leaseDuration = document.querySelector("#leaseDuration")
+    let leasePrice = document.querySelector("#leasePrice")
+    let durationSlider = document.querySelector("#duration")
+    durationSlider.addEventListener("change", (event)=>{
         leaseDuration.textContent = event.target.value + " месяц(ев)"
         leasePrice.textContent = (parseInt(event.target.value) * parseFloat(modalBoxPrice.dataset.rate)) + " ₽"
     })
 
-    inputCode = document.querySelector("#code")
-    modalBoxWhAddress = document.querySelector("#boxWhAddress")
-    modalBoxCode = document.querySelector("#boxCode")
-    modalBoxDims = document.querySelector("#boxDims")
-    modalBoxArea = document.querySelector("#boxArea")
-    modalBoxPrice = document.querySelector("#boxPrice")
+    let inputCode = document.querySelector("#code")
+    let modalBoxWhAddress = document.querySelector("#boxWhAddress")
+    let modalBoxCode = document.querySelector("#boxCode")
+    let modalBoxDims = document.querySelector("#boxDims")
+    let modalBoxArea = document.querySelector("#boxArea")
+    let modalBoxPrice = document.querySelector("#boxPrice")
 
     function fetch_data(event){
-        id = event.currentTarget.dataset.warehouse
+        let id = event.currentTarget.dataset.warehouse
 
         if (boxesTable.dataset.current == id){
             console.log("Repeated fetch attempted. Skipping.")
@@ -75,65 +79,73 @@ document.addEventListener("DOMContentLoaded", ()=>{
 
         fetch("warehouse/"+id).then((response) => response.json()).then((data)=>{
             console.log("New data arrived... rebuilding DOM")
+
+            present_boxes = data.boxes
+
             boxesTable.dataset.current = id
             boxPills = []
-            for (box of data.boxes){
-                outerContainer = document.createElement("a")
+            for(let i = 0; i<present_boxes.length; i++){
+                let outerContainer = document.createElement("a")
                 outerContainer.classList.add("js-box-row", "row", "text-decoration-none", "py-3", "px-4", "mt-5", "SelfStorage__boxlink")
-                if(box.area<3)
+                if(present_boxes[i].area<3)
                     outerContainer.classList.add("js-area-to-3")
-                if(box.area<10)
+                if(present_boxes[i].area<10)
                     outerContainer.classList.add("js-area-to-10")
                 else
                     outerContainer.classList.add("js-area-from-10")
 
-                floorCodeContainer = document.createElement("div")
+                let floorCodeContainer = document.createElement("div")
                 floorCodeContainer.classList.add("col-12", "col-md-4", "col-lg-3", "d-flex", "justify-content-center", "align-items-center")
-                floorSpan = document.createElement("span")
+                let floorSpan = document.createElement("span")
                 floorSpan.classList.add("SelfStorage_green", "fs_24", "me-2")
-                floorSpan.textContent = box.floor + " эт."
-                codeSpan = document.createElement("span")
+                floorSpan.textContent = present_boxes[i].floor + " эт."
+                let codeSpan = document.createElement("span")
                 codeSpan.classList.add("fs_24")
-                codeSpan.textContent = "№"+box.code
+                codeSpan.textContent = "№"+present_boxes[i].code
 
                 floorCodeContainer.appendChild(floorSpan)
                 floorCodeContainer.appendChild(codeSpan)
 
 
-                areaContainer = document.createElement("div")
+                let areaContainer = document.createElement("div")
                 areaContainer.classList.add("col-6", "col-md-4", "col-lg-3", "d-flex", "justify-content-center", "align-items-center")
-                areaSpan = document.createElement("span")
+                let areaSpan = document.createElement("span")
                 areaSpan.classList.add("fs_24")
-                areaSpan.textContent = box.area + " м²"
+                areaSpan.textContent = present_boxes[i].area + " м²"
 
                 areaContainer.appendChild(areaSpan)
                 
 
-                dimensionsContainer = document.createElement("div")
+                let dimensionsContainer = document.createElement("div")
                 dimensionsContainer.classList.add("col-6", "col-md-4", "col-lg-3", "d-flex", "justify-content-center", "align-items-center")
-                dimensionsSpan = document.createElement("span")
+                let dimensionsSpan = document.createElement("span")
                 dimensionsSpan.classList.add("fs_24")
-                dimensionsSpan.textContent = box.dimensions + " м"
+                dimensionsSpan.textContent = present_boxes[i].dimensions + " м"
 
                 dimensionsContainer.appendChild(dimensionsSpan)
 
 
-                rateContainer = document.createElement("div")
+                let rateContainer = document.createElement("div")
                 rateContainer.classList.add("col-12", "col-lg-3")
-                rateSpan = document.createElement("span")
+                let rateSpan = document.createElement("span")
                 rateSpan.classList.add("btn", "my-2", "w-100", "text-white", "fs_24", "SelfStorage__bg_orange", "SelfStorage__btn2_orange", "border-8")
                 rateSpan.setAttribute("data-bs-toggle", "modal")
                 rateSpan.setAttribute("data-bs-target", "#LeaseModal")
-                rateSpan.textContent = box.rate + " ₽"
+                rateSpan.textContent = present_boxes[i].rate + " ₽"
+                rateSpan.dataset.box = i
 
                 rateSpan.addEventListener("click", (event)=>{
-                    modalBoxWhAddress.textContent = box.warehouse_city+", "+box.warehouse_address
-                    modalBoxCode.textContent = "№"+box.code
-                    inputCode.value = box.code
-                    modalBoxArea.textContent = box.area + " м²"
-                    modalBoxDims.textContent = box.dimensions + " м"
-                    modalBoxPrice.textContent = box.rate + " ₽"
-                    modalBoxPrice.dataset.rate = box.rate
+                    index = parseInt(event.currentTarget.dataset.box)
+                    modalBoxWhAddress.textContent = present_boxes[index].warehouse_city+", "+present_boxes[index].warehouse_address
+                    modalBoxCode.textContent = "№"+present_boxes[index].code
+                    inputCode.value = present_boxes[index].code
+                    modalBoxArea.textContent = present_boxes[index].area + " м²"
+                    modalBoxDims.textContent = present_boxes[index].dimensions + " м"
+                    modalBoxPrice.textContent = present_boxes[index].rate + " ₽"
+                    modalBoxPrice.dataset.rate = present_boxes[index].rate
+                    durationSlider.value = 1
+                    leaseDuration.textContent = "1 месяц(ев)"
+                    leasePrice.textContent = parseFloat(present_boxes[index].rate) + " ₽"
                 })
 
                 rateContainer.appendChild(rateSpan)
