731085cb7f14ea8246d52185462a46cc67d65127
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-16 16:42:49 +0300

    Reduce SQL queries on `profile` page

--- storage/views.py
+++ storage/views.py
@@ -104,12 +104,28 @@ def avaliable_boxes(request, warehouse_id):
 
 
 def profile(request):
-    user = get_object_or_404(CustomUser, email=request.user)
+    user_leases = (
+        Lease.objects.select_related("box", "box__warehouse")
+        .filter(user__email=request.user.email)
+        .order_by("expires_on")
+    )
+    user_leases_serialized = [
+        {
+            "counter": count,
+            "warehouse_city": lease.box.warehouse.city,
+            "warehouse_address": lease.box.warehouse.address,
+            "box_number": lease.box.code,
+            "lease_from": lease.created_on,
+            "lease_till": lease.expires_on,
+        }
+        for count, lease in enumerate(user_leases, start=1)
+    ]
+
     context = {
-        "user_leases": user.leases.all(),
+        "user_leases": user_leases_serialized,
     }
 
-    return render(request, "my-rent.html", context)
+    return render(request, "my-rent.html", context=context)
 
 
 def delivery(request):
