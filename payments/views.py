446f80aca15b73b8ab8e36ef9c77519bd71333d0
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-16 18:32:25 +0300

    Accept payments

--- payments/views.py
+++ payments/views.py
@@ -1,5 +1,4 @@
 import uuid
-
 from django.shortcuts import render, redirect
 from selfstorage.settings import YOOKASSA_API_KEY, YOOKASSA_SHOP_ID
 from yookassa import Configuration, Payment
@@ -14,24 +13,30 @@ def make_payment(request):
     # https://yookassa.ru/developers/using-api/interaction-format#idempotence
     idempotence_key = uuid.uuid4()
 
-    order_details: dict = request.POST.get("order_details")
-    price_value = order_details["price"]
-    order_description = order_details["description"]
+    # order_details: dict = request.POST.get("order_details")
+    # price_value = order_details["price"]
+    # order_description = order_details["description"]
 
     payment = Payment.create(
         {
             "amount": {
-                "value": f"{price_value}.00",  # API takes 10 in "10.00" format
+                # "value": f"{price_value}.00",  # API takes 10 in "10.00" format
+                "value": "1.00",
                 "currency": "RUB",
             },
             "confirmation": {
-                "type": "redirect",
-                "return_url": "http://127.0.0.1:8000/payment",
+                "type": "embedded",
             },
             "capture": True,
-            "description": order_description,
+            # "description": order_description,
+            "description": "Some nice descritption...",
         },
         idempotency_key=str(idempotence_key),
     )
+    context = {"confirmation_token": payment.confirmation.confirmation_token}
 
-    return redirect(payment.confirmation.confirmation_url)
+    return render(request, "payment.html", context=context)
+
+
+def confirm_payment(request):
+    return render(request, "payment-confirmation.html")
