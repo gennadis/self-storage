bab7d927ea64a691f9a339895402019ce7677855
Gennady Badinov <gennady.badinov@gmail.com>
2022-06-17 16:56:36 +0300

    Send test leases reports

--- selfstorage/celery.py
+++ selfstorage/celery.py
@@ -1,21 +1,23 @@
 import os
-
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "selfstorage.settings")
 
 app = Celery("storage")
-
-# Using a string here means the worker doesn't have to serialize
-# the configuration object to child processes.
-# - namespace='CELERY' means all celery-related configuration keys
-#   should have a `CELERY_` prefix.
 app.config_from_object("django.conf:settings", namespace="CELERY")
-
-# Load task modules from all registered Django apps.
 app.autodiscover_tasks()
 
 
 @app.task(bind=True)
 def debug_task(self):
     print(f"Request: {self.request!r}")
+
+
+app.conf.beat_schedule = {
+    "send_leases_report_daily_at_midnight": {
+        "task": "storage.tasks.send_leases_report",
+        "schedule": crontab(),
+        # "schedule": crontab(hour=0, minute=0),
+    },
+}
--- storage/tasks.py
+++ storage/tasks.py
@@ -1,15 +1,33 @@
-from .models import Warehouse
-
-from celery import shared_task
-
-
-@shared_task
-def count_warehouses():
-    return Warehouse.objects.count()
+from django.template import Template, Context
+from django.core.mail import send_mail
+from django.contrib.auth import get_user_model
+from selfstorage.celery import app
+from storage.models import Lease
+from django.conf import settings
 
 
-@shared_task
-def rename_widget(warehouse_id, address):
-    warehouse = Warehouse.objects.get(id=warehouse_id)
-    warehouse.address = address
-    warehouse.save()
+REPORT_TEMPLATE = """
+Here's your current leases:
+ 
+{% for lease in leases %}
+        "{{ lease.box }}": {{ lease.created_on }} - {{ lease.expires_on }}
+
+{% endfor %}
+"""
+
+
+@app.task
+def send_leases_report():
+    for user in get_user_model().objects.all():
+        user_leases = Lease.objects.filter(user=user)
+        if not user_leases:
+            continue
+
+        template = Template(REPORT_TEMPLATE)
+        send_mail(
+            subject="Your current leases",
+            message=template.render(context=Context({"leases": user_leases})),
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            recipient_list=[user.email],
+            fail_silently=False,
+        )
