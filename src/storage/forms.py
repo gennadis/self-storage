c49dd51eaad491f5f4a7f4024269a5f9904b9791
Aosothra <arhdam@yandex.ru>
2022-06-21 14:07:51 +0300

    Make request parameters mandatory

--- src/storage/forms.py
+++ src/storage/forms.py
@@ -2,10 +2,10 @@ from django import forms
 
 
 class CreateLeaseForm(forms.Form):
-    code = forms.CharField(max_length=10)
-    duration = forms.IntegerField(min_value=1, max_value=12)
+    code = forms.CharField(max_length=10, required=True)
+    duration = forms.IntegerField(min_value=1, max_value=12, required=True)
 
 
 class RequestDeliveryForm(forms.Form):
-    lease_id = forms.IntegerField()
-    address = forms.CharField(max_length=200)
+    lease_id = forms.IntegerField(required=True)
+    address = forms.CharField(max_length=200, required=True)
