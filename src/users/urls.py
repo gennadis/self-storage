a6e48fc0722aec9a208652c6a4b33eba2b3ecf22
staskosh <staskoshenkov@rambler.ru>
2022-06-21 22:11:37 +0300

    Fix form fields according to the frontend template.

--- src/users/urls.py
+++ src/users/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("profile", views.profile, name="profile"),
     path("register", views.register, name="register"),
+    path("signup_confirmation", views.signup_confirmation, name="signup_confirmation"),
 ]
